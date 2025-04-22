a5e54aacac114fb9678c807f9a063ff7da0b021e
bortnikovaanna <bortnikovaanna@icloud.com>
2025-04-22 19:18:49 +0300

    предварительная версия

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -1,102 +1,344 @@
 #include "s21_matrix_oop.h"
+
 #include <iostream>
 
-void S21Matrix::PrintlnMatrix() const {
-    for (size_t i = 0; i < this->rows_; ++i) {
-        auto sep = "";
-        for (size_t j = 0; j < this->cols_; ++j) {
-            const size_t m = this->_GetDataIdx(i, j);
-            std::cout << sep << this->matrix_[m];
-            sep = " ";
-        }
-        std::cout << std::endl;
+// void S21Matrix::PrintlnMatrix() const {
+//     for (size_t i = 0; i < this->rows_; ++i) {
+//         auto sep = "";
+//         for (size_t j = 0; j < this->cols_; ++j) {
+//             const size_t m = this->_GetDataIdx(i, j);
+//             std::cout << sep << this->matrix_[m];
+//             sep = " ";
+//         }
+//         std::cout << std::endl;
+//     }
+// }
+
+void S21Matrix::SetRows(const int row) {
+  if (row < 1) {
+    throw std::out_of_range("Incorrect input, rows should be more than zero");
+  }
+  S21Matrix res(row, this->cols_);
+  for (int i = 0; i < row; ++i) {
+    for (int j = 0; j < (int)this->cols_; ++j) {
+      size_t m = res._GetDataIdx(i, j);
+      if (i < (int)this->rows_) {
+        size_t n = this->_GetDataIdx(i, j);
+        res.matrix_[m] = this->matrix_[n];
+      } else {
+        res.matrix_[m] = 0.0;
+      }
     }
+  }
+  *this = res;
+}
+
+void S21Matrix::SetCols(const int col) {
+  if (col < 1) {
+    throw std::out_of_range("Incorrect input, rows should be more than zero");
+  }
+  S21Matrix res(this->rows_, col);
+  for (int i = 0; i < (int)this->rows_; ++i) {
+    for (int j = 0; j < col; ++j) {
+      size_t m = res._GetDataIdx(i, j);
+      if (j < (int)this->cols_) {
+        size_t n = this->_GetDataIdx(i, j);
+        res.matrix_[m] = this->matrix_[n];
+      } else {
+        res.matrix_[m] = 0.0;
+      }
+    }
+  }
+  *this = res;
 }
 
 size_t S21Matrix::_GetDataIdx(const size_t i, const size_t j) const {
-    return i * this->cols_ + j;
+  return i * this->cols_ + j;
 }
 
-S21Matrix::ItemsPtr S21Matrix::_AllocateMatrixData(const size_t r, const size_t c) {
-    return (r * c != 0) ? new S21Matrix::Item[r * c]
-                        : nullptr;
+S21Matrix::ItemsPtr S21Matrix::_AllocateMatrixData(const size_t r,
+                                                   const size_t c) {
+  return (r * c != 0) ? new S21Matrix::Item[r * c] : nullptr;
 }
 
-void S21Matrix::_DeallocateMatrixData(ItemsPtr data) {
-    delete[] data;
+void S21Matrix::_DeallocateMatrixData(ItemsPtr data) { delete[] data; }
+
+void S21Matrix::_Deallocate() {
+  if (this->matrix_ != nullptr) {
+    _DeallocateMatrixData(this->matrix_);
+  }
 }
 
 S21Matrix::S21Matrix()
-    : S21Matrix(kDefaultRowsNum, kDefaultColsNum)
-{
-}
+    : S21Matrix(kDefaultRowsNum, kDefaultColsNum, kDefaultValue) {}
 
 S21Matrix::S21Matrix(const size_t r, const size_t c, const Item& init_value)
-    : rows_(r)
-    , cols_(c)
-    , matrix_(_AllocateMatrixData(r, c))
-{
-    for (size_t i = 0; i < r * c; ++i) {
-        this->matrix_[i] = init_value;
-    }
+    : rows_(r), cols_(c), matrix_(_AllocateMatrixData(r, c)) {
+  if (r == 0 || c == 0) {
+    throw std::out_of_range(
+        "Incorrect input, rows/cols should be more than zero");
+  }
+  for (size_t i = 0; i < r * c; ++i) {
+    this->matrix_[i] = init_value;
+  }
 }
 
 S21Matrix::S21Matrix(const S21Matrix& other)
-    : rows_(other.rows_)
-    , cols_(other.cols_)
-    , matrix_(_AllocateMatrixData(rows_, cols_))
-{
-    for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
-        this->matrix_[i] = other.matrix_[i];
-    }
+    : rows_(other.rows_),
+      cols_(other.cols_),
+      matrix_(_AllocateMatrixData(rows_, cols_)) {
+  for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
+    this->matrix_[i] = other.matrix_[i];
+  }
 }
 
 S21Matrix::S21Matrix(S21Matrix&& other)
-    : rows_(other.rows_)
-    , cols_(other.cols_)
-    , matrix_(other.matrix_)  
-{
-    other.rows_ = 0;
-    other.cols_ = 0;
-    other.matrix_ = nullptr;
+    : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
+  other.rows_ = 0;
+  other.cols_ = 0;
+  other.matrix_ = nullptr;
 }
 
-S21Matrix::~S21Matrix() {
-    this->_Deallocate();
-}
-
-void S21Matrix::_Deallocate() {
-    if (this->matrix_ != nullptr) {
-        _DeallocateMatrixData(this->matrix_);
-    }
-}
+S21Matrix::~S21Matrix() { this->_Deallocate(); }
 
 S21Matrix& S21Matrix::operator=(const S21Matrix& other) {
-    if (&other != this) {
-        this->_Deallocate();
+  if (&other != this) {
+    this->_Deallocate();
 
-        this->rows_ = other.rows_;
-        this->cols_ = other.cols_;
-        this->matrix_ = _AllocateMatrixData(this->rows_, this->cols_);
+    this->rows_ = other.rows_;
+    this->cols_ = other.cols_;
+    this->matrix_ = _AllocateMatrixData(this->rows_, this->cols_);
 
-        for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
-            this->matrix_[i] = other.matrix_[i];
-        }
+    for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
+      this->matrix_[i] = other.matrix_[i];
     }
-    return *this;
+  }
+  return *this;
 }
 
 S21Matrix& S21Matrix::operator=(S21Matrix&& other) {
-    if (&other != this) {
-        this->_Deallocate();
+  if (&other != this) {
+    this->_Deallocate();
 
-        this->rows_ = other.rows_;
-        this->cols_ = other.cols_;
-        this->matrix_ = other.matrix_;
+    this->rows_ = other.rows_;
+    this->cols_ = other.cols_;
+    this->matrix_ = other.matrix_;
 
-        other.rows_ = 0;
-        other.cols_ = 0;
-        other.matrix_ = nullptr;
-    }
-    return *this;
+    other.rows_ = 0;
+    other.cols_ = 0;
+    other.matrix_ = nullptr;
+  }
+  return *this;
 }
+
+S21Matrix::Item& S21Matrix::operator()(const size_t i, const size_t j) {
+  if (i >= rows_ || j >= cols_ || i < 0 || j < 0) {
+    throw std::out_of_range("Index out of range");
+  }
+  return this->matrix_[_GetDataIdx(i, j)];
+}
+
+const S21Matrix::Item& S21Matrix::operator()(const size_t i,
+                                             const size_t j) const {
+  return this->matrix_[_GetDataIdx(i, j)];
+}
+
+S21Matrix S21Matrix::operator+(const S21Matrix& other) const {
+  S21Matrix res(*this);
+  res.SumMatrix(other);
+  return res;
+}
+
+S21Matrix S21Matrix::operator-(const S21Matrix& other) const {
+  S21Matrix res(*this);
+  res.SubMatrix(other);
+  return res;
+}
+
+S21Matrix S21Matrix::operator*(const S21Matrix& other) const {
+  S21Matrix res(*this);
+  res.MulMatrix(other);
+  return res;
+}
+
+S21Matrix S21Matrix::operator*(const Item num) const {
+  S21Matrix res(*this);
+  res.MulNumber(num);
+  return res;
+}
+
+bool S21Matrix::operator==(const S21Matrix& other) const {
+  S21Matrix res(*this);
+  return res.EqMatrix(other);
+}
+
+S21Matrix& S21Matrix::operator+=(const S21Matrix& other) {
+  SumMatrix(other);
+  return *this;
+}
+
+S21Matrix& S21Matrix::operator-=(const S21Matrix& other) {
+  SubMatrix(other);
+  return *this;
+}
+
+S21Matrix& S21Matrix::operator*=(const S21Matrix& other) {
+  MulMatrix(other);
+  return *this;
+}
+
+S21Matrix& S21Matrix::operator*=(const Item num) {
+  MulNumber(num);
+  return *this;
+}
+
+bool S21Matrix::EqMatrix(const S21Matrix& other) const {
+  bool res = true;
+
+  if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
+    res = false;
+  } else {
+    for (size_t i = 0; i < this->rows_ * this->cols_ && res; ++i) {
+      if (std::fabs(this->matrix_[i] - other.matrix_[i]) > 1e-6) {
+        res = false;
+      }
+    }
+  }
+  return res;
+}
+
+void S21Matrix::SumMatrix(const S21Matrix& other) {
+  if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
+    throw std::invalid_argument("Invalid matrix's sizes");
+  }
+  for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
+    this->matrix_[i] += other.matrix_[i];
+  }
+}
+
+void S21Matrix::SubMatrix(const S21Matrix& other) {
+  if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
+    throw std::invalid_argument("Invalid matrix's sizes");
+  }
+  for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
+    this->matrix_[i] -= other.matrix_[i];
+  }
+}
+
+void S21Matrix::MulNumber(const Item num) {
+  for (size_t i = 0; i < this->rows_ * this->cols_; ++i) {
+    this->matrix_[i] *= num;
+  }
+}
+
+void S21Matrix::MulMatrix(const S21Matrix& other) {
+  if (this->cols_ != other.rows_) {
+    throw std::invalid_argument("Invalid matrix's sizes");
+  }
+  S21Matrix res(this->rows_, other.cols_);
+  for (size_t i = 0; i < this->rows_; ++i) {
+    for (size_t j = 0; j < other.cols_; ++j) {
+      for (size_t k = 0; k < this->cols_; ++k) {
+        size_t r = res._GetDataIdx(i, j);
+        size_t t = this->_GetDataIdx(i, k);
+        size_t o = other._GetDataIdx(k, j);
+        res.matrix_[r] += this->matrix_[t] * other.matrix_[o];
+      }
+    }
+  }
+  *this = res;
+}
+
+S21Matrix S21Matrix::Transpose() const {
+  S21Matrix res(this->cols_, this->rows_);
+  for (size_t i = 0; i < this->rows_; ++i) {
+    for (size_t j = 0; j < this->cols_; ++j) {
+      size_t m = this->_GetDataIdx(i, j);
+      size_t n = res._GetDataIdx(j, i);
+      res.matrix_[n] = this->matrix_[m];
+    }
+  }
+  return res;
+}
+
+S21Matrix S21Matrix::_Minor(size_t row, size_t col) const {
+  S21Matrix MinorMatrix(this->rows_ - 1, this->cols_ - 1);
+  size_t MinorI = 0;
+  for (size_t i = 0; i < this->rows_; ++i) {
+    if (i == row) continue;
+    size_t MinorJ = 0;
+    for (size_t j = 0; j < this->cols_; ++j) {
+      if (j == col) continue;
+      size_t m = MinorMatrix._GetDataIdx(MinorI, MinorJ);
+      size_t o = this->_GetDataIdx(i, j);
+      MinorMatrix.matrix_[m] = this->matrix_[o];
+      MinorJ++;
+    }
+    MinorI++;
+  }
+  return MinorMatrix;
+}
+
+S21Matrix::Item S21Matrix::Determinant() const {
+  if (this->rows_ != this->cols_) {
+    throw std::domain_error("Matrix isn't square");
+  }
+  Item res = 0.0;
+  if (this->rows_ == 1) {
+    res = this->matrix_[0];
+  } else if (this->rows_ == 2) {
+    res = this->matrix_[0] * this->matrix_[3] -
+          this->matrix_[1] * this->matrix_[2];
+  } else {
+    for (size_t i = 0; i < this->cols_; ++i) {
+      S21Matrix MinorMatrix = this->_Minor(0, i);
+      size_t t = this->_GetDataIdx(0, i);
+      res += this->matrix_[t] * std::pow(-1.0, i) * MinorMatrix.Determinant();
+    }
+  }
+  return res;
+}
+
+S21Matrix S21Matrix::CalcComplements() const {
+  if (this->rows_ != this->cols_) {
+    throw std::domain_error("Matrix isn't square");
+  }
+  S21Matrix res(this->rows_, this->cols_);
+  for (size_t i = 0; i < this->rows_; ++i) {
+    for (size_t j = 0; j < this->cols_; ++j) {
+      S21Matrix MinorMatrix = this->_Minor(i, j);
+      size_t n = res._GetDataIdx(i, j);
+      res.matrix_[n] = std::pow(-1.0, i + j) * MinorMatrix.Determinant();
+    }
+  }
+  return res;
+}
+
+S21Matrix S21Matrix::InverseMatrix() const {
+  Item d = this->Determinant();
+  if (std::fabs(d) < 1e-6) {
+    throw std::invalid_argument("Determinant can't be equal zero");
+  }
+  S21Matrix res(this->rows_, this->cols_);
+  if (this->rows_ == 1) {
+    res.matrix_[0] = 1.0 / this->matrix_[0];
+  } else {
+    S21Matrix comp = this->CalcComplements();
+    res = comp.Transpose();
+    res.MulNumber(1.0 / d);
+  }
+  return res;
+}
+
+// int main () {
+
+//     S21Matrix a(4, 2, 1.0);
+//     a.PrintlnMatrix();
+//     S21Matrix b(4, 4, 2.0);
+//     b.PrintlnMatrix();
+//     S21Matrix c = b.InverseMatrix();
+//     c.PrintlnMatrix();
+//     // S21Matrix::Item c = b.Determinant();
+//     // std::cout << c << std::endl;
+//     return 0;
+// }
---
+++ src/tests/s21_matrix_tests.cpp
@@ -0,0 +1,436 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <iostream>
+
+#include "s21_matrix_oop.h"
+
+// Тесты конструкторов
+TEST(ConstructorTest, DefaultConstructor) {
+  S21Matrix matrix1;
+  EXPECT_EQ(matrix1.GetRows(), 3ul);
+  EXPECT_EQ(matrix1.GetCols(), 3ul);
+}
+
+TEST(ConstructorTest, ParameterizedConstructor) {
+  S21Matrix matrix1(4ul, 5ul);
+  EXPECT_EQ(matrix1.GetRows(), 4ul);
+  EXPECT_EQ(matrix1.GetCols(), 5ul);
+}
+
+TEST(ConstructorTest, CopyConstructor) {
+  S21Matrix matrix1(2ul, 3ul);
+  matrix1(0, 0) = 1.0;
+  S21Matrix matrix2(matrix1);
+  EXPECT_TRUE(matrix1 == matrix2);
+}
+
+TEST(ConstructorTest, MoveConstructor) {
+  S21Matrix matrix1(2ul, 3ul);
+  matrix1(0, 0) = 1.0;
+  S21Matrix copy(matrix1);
+  S21Matrix moved(std::move(matrix1));
+  EXPECT_TRUE(moved == copy);
+  EXPECT_EQ(matrix1.GetRows(), 0ul);
+  EXPECT_EQ(matrix1.GetCols(), 0ul);
+}
+
+// Тесты операторов присваивания
+TEST(AssignmentOperatorTest, CopySelfAssignment) {
+  S21Matrix matrix1(2ul, 2ul);
+  matrix1(0, 0) = 1.0;
+  S21Matrix matrix2 = matrix1;
+  EXPECT_TRUE(matrix1 == matrix2);
+}
+
+TEST(AssignmentOperatorTest, MoveSelfMove) {
+  S21Matrix matrix1(2ul, 2ul);
+  matrix1(0, 0) = 1.0;
+  S21Matrix copy = matrix1;
+
+  S21Matrix temp = std::move(matrix1);
+  EXPECT_TRUE(temp == copy);
+  EXPECT_EQ(matrix1.GetRows(), 0ul);
+  EXPECT_EQ(matrix1.GetCols(), 0ul);
+}
+
+TEST(EqMatrixTest, DifferentDimensions) {
+    S21Matrix matrix1(3, 3);
+    S21Matrix matrix2(3, 4);
+    EXPECT_FALSE(matrix1 == matrix2);
+}
+
+TEST(EqMatrixTest, SmallDifference) {
+    S21Matrix matrix1(3, 3);
+    S21Matrix matrix2(3, 3);
+    matrix1(0, 0) = 1.0;
+    matrix2(0, 0) = 1.0 + 1e-7;
+    EXPECT_TRUE(matrix1 == matrix2);
+}
+
+TEST(EqMatrixTest, SignificantDifference) {
+    S21Matrix matrix1(3, 3);
+    S21Matrix matrix2(3, 3);
+    matrix1(0, 0) = 1.0;
+    matrix2(0, 0) = 2.0;
+    EXPECT_FALSE(matrix1 == matrix2);
+}
+
+TEST(OperatorTest, PlusEquals) {
+  S21Matrix matrix1(2, 2);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(1, 0) = 3;
+  matrix1(1, 1) = 4;
+
+  S21Matrix matrix2(2, 2);
+  matrix2(0, 0) = 2;
+  matrix2(0, 1) = 3;
+  matrix2(1, 0) = 4;
+  matrix2(1, 1) = 5;
+
+  matrix1 += matrix2;
+
+  EXPECT_DOUBLE_EQ(matrix1(0, 0), 3.0);
+  EXPECT_DOUBLE_EQ(matrix1(0, 1), 5.0);
+  EXPECT_DOUBLE_EQ(matrix1(1, 0), 7.0);
+  EXPECT_DOUBLE_EQ(matrix1(1, 1), 9.0);
+}
+
+TEST(OperatorTest, MinusEquals) {
+  S21Matrix matrix1(2, 2);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(1, 0) = 3;
+  matrix1(1, 1) = 4;
+
+  S21Matrix matrix2(2, 2);
+  matrix2(0, 0) = 2;
+  matrix2(0, 1) = 3;
+  matrix2(1, 0) = 4;
+  matrix2(1, 1) = 5;
+
+  matrix1 -= matrix2;
+
+  EXPECT_DOUBLE_EQ(matrix1(0, 0), -1.0);
+  EXPECT_DOUBLE_EQ(matrix1(0, 1), -1.0);
+  EXPECT_DOUBLE_EQ(matrix1(1, 0), -1.0);
+  EXPECT_DOUBLE_EQ(matrix1(1, 1), -1.0);
+}
+
+TEST(OperatorTest, MultiplyEqualsScalar) {
+  S21Matrix matrix(2, 2);
+  matrix(0, 0) = 1;
+  matrix(0, 1) = 2;
+  matrix(1, 0) = 3;
+  matrix(1, 1) = 4;
+
+  matrix *= 2.0;
+
+  EXPECT_DOUBLE_EQ(matrix(0, 0), 2.0);
+  EXPECT_DOUBLE_EQ(matrix(0, 1), 4.0);
+  EXPECT_DOUBLE_EQ(matrix(1, 0), 6.0);
+  EXPECT_DOUBLE_EQ(matrix(1, 1), 8.0);
+}
+
+// Тесты граничных значений
+TEST(EdgeCaseTest, ZeroSizeMatrix) {
+  EXPECT_THROW(S21Matrix matrix1(0ul, 0ul), std::out_of_range);
+}
+
+TEST(EdgeCaseTest, LargeMatrix) {
+  S21Matrix matrix1(1000ul, 1000ul);
+  EXPECT_EQ(matrix1.GetRows(), 1000ul);
+  EXPECT_EQ(matrix1.GetCols(), 1000ul);
+}
+
+// Тесты методов
+TEST(MethodTest, SetRowsIncrease) {
+  S21Matrix matrix1(2ul, 3ul);
+  matrix1.SetRows(4ul);
+  EXPECT_EQ(matrix1.GetRows(), 4ul);
+  for (size_t i = 2; i < 4; ++i) {
+    for (size_t j = 0; j < 3; ++j) {
+      EXPECT_DOUBLE_EQ(matrix1(i, j), 0.0);
+    }
+  }
+}
+
+TEST(MethodTest, SetRowsDecrease) {
+  S21Matrix matrix1(4ul, 3ul);
+  matrix1.SetRows(2ul);
+  EXPECT_EQ(matrix1.GetRows(), 2ul);
+}
+
+TEST(SetRowsTest, InvalidRowValue) {
+    S21Matrix matrix(3, 3);
+    EXPECT_THROW(matrix.SetRows(0), std::out_of_range);
+    EXPECT_THROW(matrix.SetRows(-1), std::out_of_range);
+}
+
+TEST(SetColsTest, IncreaseSize) {
+  S21Matrix matrix(2, 2);
+  matrix.SetCols(3);
+
+  EXPECT_EQ(matrix.GetCols(), 3);
+  EXPECT_DOUBLE_EQ(matrix(0, 2), 0.0);
+  EXPECT_DOUBLE_EQ(matrix(1, 2), 0.0);
+}
+
+TEST(SetColsTest, DecreaseSize) {
+  S21Matrix matrix(2, 3);
+  matrix.SetCols(2);
+
+  EXPECT_EQ(matrix.GetCols(), 2);
+}
+
+TEST(SetColsTest, InvalidColValue) {
+    S21Matrix matrix(3, 3);
+    EXPECT_THROW(matrix.SetCols(0), std::out_of_range);
+    EXPECT_THROW(matrix.SetCols(-1), std::out_of_range);
+}
+
+TEST(IndexationTest, OutOfBounds) {
+    S21Matrix matrix(3, 3);
+    EXPECT_THROW(matrix(3, 2), std::out_of_range); // Row out of bounds
+    EXPECT_THROW(matrix(2, 3), std::out_of_range); // Column out of bounds
+    EXPECT_THROW(matrix(-1, 2), std::out_of_range); // Negative row index
+    EXPECT_THROW(matrix(2, -1), std::out_of_range); // Negative column index
+}
+
+TEST(SumMatrixTest, MismatchedDimensions) {
+    S21Matrix matrix1(3, 3);
+    S21Matrix matrix2(3, 4);
+    EXPECT_THROW(matrix1.SumMatrix(matrix2), std::invalid_argument);
+}
+
+TEST(SubMatrixTest, MismatchedDimensions) {
+    S21Matrix matrix1(3, 3);
+    S21Matrix matrix2(3, 4);
+    EXPECT_THROW(matrix1.SubMatrix(matrix2), std::invalid_argument);
+}
+
+TEST(SubMatrixTest, Basic) {
+  S21Matrix matrix1(2, 2);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(1, 0) = 3;
+  matrix1(1, 1) = 4;
+
+  S21Matrix matrix2(2, 2);
+  matrix2(0, 0) = 2;
+  matrix2(0, 1) = 3;
+  matrix2(1, 0) = 4;
+  matrix2(1, 1) = 5;
+
+  matrix1.SubMatrix(matrix2);
+
+  EXPECT_DOUBLE_EQ(matrix1(0, 0), -1.0);
+  EXPECT_DOUBLE_EQ(matrix1(0, 1), -1.0);
+  EXPECT_DOUBLE_EQ(matrix1(1, 0), -1.0);
+  EXPECT_DOUBLE_EQ(matrix1(1, 1), -1.0);
+}
+
+TEST(SubMatrixTest, SizeMismatchException) {
+  S21Matrix matrix1(2, 2);
+  S21Matrix matrix2(3, 3);
+  EXPECT_THROW(matrix1.SubMatrix(matrix2), std::invalid_argument);
+}
+
+TEST(InverseMatrixTest, Basic2x2) {
+  S21Matrix matrix(2, 2);
+  matrix(0, 0) = 1;
+  matrix(0, 1) = 2;
+  matrix(1, 0) = 3;
+  matrix(1, 1) = 4;
+
+  S21Matrix result = matrix.InverseMatrix();
+  EXPECT_NEAR(result(0, 0), -2.0, 1e-6);
+  EXPECT_NEAR(result(0, 1), 1.0, 1e-6);
+  EXPECT_NEAR(result(1, 0), 1.5, 1e-6);
+  EXPECT_NEAR(result(1, 1), -0.5, 1e-6);
+}
+
+TEST(InverseMatrixTest, ZeroDeterminantException) {
+  S21Matrix matrix(2, 2);
+  matrix(0, 0) = 1;
+  matrix(0, 1) = 2;
+  matrix(1, 0) = 2;
+  matrix(1, 1) = 4;
+
+  EXPECT_THROW(matrix.InverseMatrix(), std::invalid_argument);
+}
+
+TEST(InverseMatrixTest, NonSquareMatrixException) {
+  S21Matrix matrix(2, 3);
+  EXPECT_THROW(matrix.InverseMatrix(), std::domain_error);
+}
+
+TEST(CalcComplementsTest, Basic3x3) {
+  S21Matrix matrix(3, 3);
+  matrix(0, 0) = 1;
+  matrix(0, 1) = 2;
+  matrix(0, 2) = 3;
+  matrix(1, 0) = 4;
+  matrix(1, 1) = 5;
+  matrix(1, 2) = 6;
+  matrix(2, 0) = 7;
+  matrix(2, 1) = 8;
+  matrix(2, 2) = 9;
+
+  S21Matrix result = matrix.CalcComplements();
+  EXPECT_DOUBLE_EQ(result(0, 0), -3.0);
+  EXPECT_DOUBLE_EQ(result(0, 1), 6.0);
+  EXPECT_DOUBLE_EQ(result(0, 2), -3.0);
+  EXPECT_DOUBLE_EQ(result(1, 0), 6.0);
+  EXPECT_DOUBLE_EQ(result(1, 1), -12.0);
+  EXPECT_DOUBLE_EQ(result(1, 2), 6.0);
+  EXPECT_DOUBLE_EQ(result(2, 0), -3.0);
+  EXPECT_DOUBLE_EQ(result(2, 1), 6.0);
+  EXPECT_DOUBLE_EQ(result(2, 2), -3.0);
+}
+
+TEST(CalcComplementsTest, NonSquareMatrixException) {
+  S21Matrix matrix(2, 3);
+  EXPECT_THROW(matrix.CalcComplements(), std::domain_error);
+}
+
+TEST(CalcComplementsTest, NonSquareMatrix) {
+    S21Matrix matrix(3, 4);
+    EXPECT_THROW(matrix.CalcComplements(), std::domain_error);
+}
+
+TEST(MethodTest, Determinant) {
+  S21Matrix matrix1(2ul, 2ul);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(1, 0) = 3;
+  matrix1(1, 1) = 4;
+  EXPECT_DOUBLE_EQ(matrix1.Determinant(), -2.0);
+}
+
+TEST(DeterminantTest, Basic2x2) {
+  S21Matrix matrix(2, 2);
+  matrix(0, 0) = 1;
+  matrix(0, 1) = 2;
+  matrix(1, 0) = 3;
+  matrix(1, 1) = 4;
+
+  EXPECT_DOUBLE_EQ(matrix.Determinant(), -2.0);
+}
+
+TEST(DeterminantTest, Basic3x3) {
+  S21Matrix matrix(3, 3);
+  matrix(0, 0) = 6;
+  matrix(0, 1) = 1;
+  matrix(0, 2) = 1;
+  matrix(1, 0) = 4;
+  matrix(1, 1) = -2;
+  matrix(1, 2) = 5;
+  matrix(2, 0) = 2;
+  matrix(2, 1) = 8;
+  matrix(2, 2) = 7;
+
+  EXPECT_DOUBLE_EQ(matrix.Determinant(), -306.0);
+}
+
+TEST(DeterminantTest, ZeroDeterminant) {
+  S21Matrix matrix(3, 3);
+  matrix(0, 0) = 1;
+  matrix(0, 1) = 2;
+  matrix(0, 2) = 3;
+  matrix(1, 0) = 4;
+  matrix(1, 1) = 5;
+  matrix(1, 2) = 6;
+  matrix(2, 0) = 7;
+  matrix(2, 1) = 8;
+  matrix(2, 2) = 9;
+
+  EXPECT_DOUBLE_EQ(matrix.Determinant(), 0.0);
+}
+
+TEST(DeterminantTest, NonSquareMatrix) {
+    S21Matrix matrix(3, 4);
+    EXPECT_THROW(matrix.Determinant(), std::domain_error);
+}
+
+TEST(DeterminantTest, NonSquareMatrixException) {
+  S21Matrix matrix(2, 3);
+  EXPECT_THROW(matrix.Determinant(), std::domain_error);
+}
+
+TEST(MethodTest, Transpose) {
+  S21Matrix matrix1(2ul, 3ul);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(0, 2) = 3;
+  matrix1(1, 0) = 4;
+  matrix1(1, 1) = 5;
+  matrix1(1, 2) = 6;
+
+  S21Matrix transposed = matrix1.Transpose();
+  EXPECT_EQ(transposed.GetRows(), 3ul);
+  EXPECT_EQ(transposed.GetCols(), 2ul);
+  EXPECT_DOUBLE_EQ(transposed(0, 0), 1.0);
+  EXPECT_DOUBLE_EQ(transposed(0, 1), 4.0);
+  EXPECT_DOUBLE_EQ(transposed(1, 0), 2.0);
+  EXPECT_DOUBLE_EQ(transposed(1, 1), 5.0);
+  EXPECT_DOUBLE_EQ(transposed(2, 0), 3.0);
+  EXPECT_DOUBLE_EQ(transposed(2, 1), 6.0);
+}
+
+// Тесты операторов
+TEST(OperatorTest, Addition) {
+  S21Matrix matrix1(2ul, 2ul);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(1, 0) = 3;
+  matrix1(1, 1) = 4;
+
+  S21Matrix matrix2(2ul, 2ul);
+  matrix2(0, 0) = 5;
+  matrix2(0, 1) = 6;
+  matrix2(1, 0) = 7;
+  matrix2(1, 1) = 8;
+
+  S21Matrix result = matrix1 + matrix2;
+  EXPECT_DOUBLE_EQ(result(0, 0), 6.0);
+  EXPECT_DOUBLE_EQ(result(0, 1), 8.0);
+  EXPECT_DOUBLE_EQ(result(1, 0), 10.0);
+  EXPECT_DOUBLE_EQ(result(1, 1), 12.0);
+}
+
+TEST(OperatorTest, Multiplication) {
+  S21Matrix matrix1(2ul, 3ul);
+  matrix1(0, 0) = 1;
+  matrix1(0, 1) = 2;
+  matrix1(0, 2) = 3;
+  matrix1(1, 0) = 4;
+  matrix1(1, 1) = 5;
+  matrix1(1, 2) = 6;
+
+  S21Matrix matrix2(3ul, 2ul);
+  matrix2(0, 0) = 7;
+  matrix2(0, 1) = 8;
+  matrix2(1, 0) = 9;
+  matrix2(1, 1) = 10;
+  matrix2(2, 0) = 11;
+  matrix2(2, 1) = 12;
+
+  S21Matrix result = matrix1 * matrix2;
+  EXPECT_DOUBLE_EQ(result(0, 0), 58.0);
+  EXPECT_DOUBLE_EQ(result(0, 1), 64.0);
+  EXPECT_DOUBLE_EQ(result(1, 0), 139.0);
+  EXPECT_DOUBLE_EQ(result(1, 1), 154.0);
+}
+
+TEST(MulMatrixTest, MismatchedDimensions) {
+    S21Matrix matrix1(3, 4);
+    S21Matrix matrix2(5, 3);
+    EXPECT_THROW(matrix1.MulMatrix(matrix2), std::invalid_argument);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
